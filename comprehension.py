48e48f9c4288d8aa910a3a91fa95ad6b8254ea32
Abel Nagy <nagy9219abel@gmail.com>
2017-07-03 11:41:59 +0200

    second commit

--- comprehension.py
+++ comprehension.py
@@ -1,33 +1,33 @@
-import random
-'''helo'''
-guessesTaken = 0
+import random  # imports the random module
 
-print('Hello! What is your name?')
-myName = input()
+guessesTaken = 0  # assign 0 to the variable guessesTaken
 
-number = random.randint(1, 20)
-print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
+print('Hello! What is your name?')  # prints out string in the argument
+myName = input()  # declares the variable myName, sets its value to an user input value
 
-while guessesTaken < 6:
-    print('Take a guess.')
-    guess = input()
-    guess = int(guess)
+number = random.randint(1, 20)  # randomizes an integer between 1 and 20 and assign it to the number variable
+print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')  # prints out the message with the number variable
 
-    guessesTaken += 1
+while guessesTaken < 6:  # loop. while the guessesTaken variable's value is less then 6, the following indented code block will repeatedly execute
+    print('Take a guess.')  # prints out message (string)
+    guess = input()  # declares the variable guess, sets its value to an user input value (string)
+    guess = int(guess)  # converts guess variable into an integer type (the input() is a string by default)
 
-    if guess < number:
-        print('Your guess is too low.')
+    guessesTaken += 1  # increases guessesTaken's value by 1
 
-    if guess > number:
-        print('Your guess is too high.')
+    if guess < number:  # if the value of guess is less then the value of number the following indented block of code will execute
+        print('Your guess is too low.')  # prints out a message (string)
 
-    if guess == number:
-        break
+    if guess > number:  # if the value of guess is greater then the value of number the following indented block of code will execute
+        print('Your guess is too high.')  # prints out a message (sring)
 
-if guess == number:
-    guessesTaken = str(guessesTaken)
-    print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')
+    if guess == number:  # if the value of guess is equal to the value of number the following indented block of code will execute
+        break  # breaks, stops the execution of the while loop, execution goes to the following code lines
 
-if guess != number:
-    number = str(number)
-    print('Nope. The number I was thinking of was ' + number)
+if guess == number:  # if the value of guess is equal to the value of number the following indented block of code will execute
+    guessesTaken = str(guessesTaken)  # converts guessesTaken variable into an integer type
+    print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')  # prints out message and the values of the variables in the argument (everything as string)
+
+if guess != number:  # if the value of guess is not equal to the value of number the following indtented block of code will execute
+    number = str(number)  # converts the number variable into a string type
+    print('Nope. The number I was thinking of was ' + number)  # prints out a message and a the number variable's value (string)
